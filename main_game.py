68d5cbd5acc24973f02ce82ad18ced76ca936a5c
BeizzaKasha <nadavc444@gmail.com>
2021-12-22 13:07:25 +0200

    collision all works

--- main_game.py
+++ main_game.py
@@ -49,14 +49,22 @@ class Player(pygame.sprite.Sprite):
         mouse = pygame.mouse.get_pressed(3)
         val = "successful"
         pressed_keys = pygame.key.get_pressed()
-        if pressed_keys[K_w] and not self.rect.top - self.rect.height / 3 < 0 or pygame.sprite.spritecollideany(self, walls):
+        if pressed_keys[K_w] and not self.rect.top - self.rect.height / 3 < 0:
             self.rect.move_ip(0, -self.speed)
-        if pressed_keys[K_s] and not self.rect.bottom + self.rect.height / 3 > SCREEN_HEIGHT or pygame.sprite.spritecollideany(self, walls):
+            if pygame.sprite.spritecollideany(self, walls):
+                self.rect.move_ip(0, self.speed)
+        if pressed_keys[K_s] and not self.rect.bottom + self.rect.height / 3 > SCREEN_HEIGHT:
             self.rect.move_ip(0, self.speed)
-        if pressed_keys[K_a] and not (self.rect.left - self.rect.width / 3 < 0 or pygame.sprite.spritecollideany(self, walls)):
+            if pygame.sprite.spritecollideany(self, walls):
+                self.rect.move_ip(0, -self.speed)
+        if pressed_keys[K_a] and not (self.rect.left - self.rect.width / 3 < 0):
             self.rect.move_ip(-self.speed, 0)
-        if pressed_keys[K_d] and not (self.rect.right + self.rect.width / 3 > SCREEN_WIDTH or pygame.sprite.spritecollideany(self, walls)):
+            if pygame.sprite.spritecollideany(self, walls):
+                self.rect.move_ip(self.speed, 0)
+        if pressed_keys[K_d] and not (self.rect.right + self.rect.width / 3 > SCREEN_WIDTH):
             self.rect.move_ip(self.speed, 0)
+            if pygame.sprite.spritecollideany(self, walls):
+                self.rect.move_ip(-self.speed, 0)
         if mouse[0] and self.fire_wait <= 0:
             val = self.fire()
             self.fire_wait = 60
@@ -92,14 +100,14 @@ class Walls(pygame.sprite.Sprite):
     def __init__(self):
         super(Walls, self).__init__()
         self.center = (random.randint(1, 801), random.randint(1, 601))
-        self.size = (random.randint(20, 201), random.randint(20, 201))
-        self.angle = random.randint(0, 360)
+        self.size = (random.randint(50, 201), random.randint(50, 201))
+        self.angle = random.randint(0, 4) * 90
         self.rectangle = pygame.Surface(self.size, pygame.SRCALPHA)
         self.rectangle.fill(pygame.Color('dark gray'))
         self.rect = self.rectangle.get_rect()
         self.rect.move_ip(self.center)
-        self.rot_image = pygame.transform.rotate(self.rectangle, self.angle)
-        self.rot_image_rect = self.rot_image.get_rect(center=self.rect.center)
+        self.rectangle = pygame.transform.rotate(self.rectangle, self.angle)
+        self.rect = self.rectangle.get_rect(center=self.rect.center)
 
 
 pygame.init()
@@ -114,10 +122,10 @@ clock = pygame.time.Clock()
 def main():
     player = Player()
     enemies = pygame.sprite.Group()
-    all_sprites = []
-    for i in range(random.randint(5, 10)):
+    all_sprites = pygame.sprite.Group()
+    for i in range(random.randint(10, 15)):
         wall = Walls()
-        all_sprites.append(wall)
+        all_sprites.add(wall)
 
     running = True
 
@@ -142,12 +150,14 @@ def main():
         enemies.update()
 
         for entity in all_sprites:
-            screen.blit(entity.rot_image, entity.rot_image_rect.topleft)
+            screen.blit(entity.rectangle, entity.rect.topleft)
 
         screen.blit(player.rot_image, player.rot_image_rect.topleft)
 
         for bullet in enemies:
             pygame.draw.circle(screen, (255, 255, 255), (bullet.rot_image_rect.x, bullet.rot_image_rect.y), 5)
+            if pygame.sprite.spritecollideany(bullet, all_sprites):
+                bullet.kill()
 
         if pygame.sprite.spritecollideany(player, enemies):
             player.kill()
